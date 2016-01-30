a0f872cf59fc51b3b091312268f67df040ca1dfd
Camille Scott <camille.scott.w@gmail.com>
2016-01-30 00:05:01 -0800

    Init commit with code and data

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/ctree1/")
+def ctree1():
+    return render_template("collapsible-tree.html")
+
+
+@app.route("/ctree2/")
+def ctree2():
+    return render_template("collapsible-tree-2.html")
+
+
+@app.route("/cctree/")
+def cctree():
+    return render_template("collapsible-circular.html")
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=5000,debug=True)
